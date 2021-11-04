420c613b47064c07b641eb3fd0da7f03bb2e3511
Thomas Wilde <thomas@isg.cs.ovgu.de>
2021-11-04 17:14:21 +0100

    ADD: Canvas.java

---
+++ core/src/com/tw/paintbots/Canvas.java
@@ -0,0 +1,9 @@
+package com.tw.paintbots;
+
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Blending;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+
+public class Canvas {
+
+}
