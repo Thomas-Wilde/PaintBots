ab45f4f7f61456fc1d941332c272c1df951b3b06
Thomas Wilde <thomas@isg.cs.ovgu.de>
2021-11-09 18:06:13 +0100

    ADD: class PlayerState

---
+++ core/src/com/tw/paintbots/PlayerState.java
@@ -0,0 +1,9 @@
+package com.tw.paintbots;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class PlayerState {
+  public Vector2 pos = new Vector2(0.0f, 0.0f);
+  public Vector2 dir = new Vector2(0.0f, 0.0f);
+  public PlayerType type = PlayerType.NONE;
+}
