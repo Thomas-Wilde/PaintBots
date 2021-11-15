2c5b4e1bfc619320b1886322befb4069ea8f720f
Thomas Wilde <ThomasWilde1984@gmail.com>
2021-11-15 20:41:32 +0100

    CHANGED: GameSettings has 4 players

--- core/src/com/tw/paintbots/GameSettings.java
+++ core/src/com/tw/paintbots/GameSettings.java
@@ -3,11 +3,14 @@ package com.tw.paintbots;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameSettings {
-  public PlayerType[] player_types = {PlayerType.HUMAN, PlayerType.HUMAN};
+  public PlayerType[] player_types =
+      {PlayerType.HUMAN, PlayerType.HUMAN, PlayerType.HUMAN, PlayerType.HUMAN};
   public Vector2[] start_positions =
-      {new Vector2(250.0f, 500.0f), new Vector2(500.0f, 500.0f),};
+      {new Vector2(250.0f, 500.0f), new Vector2(500.0f, 500.0f),
+          new Vector2(325.0f, 750.0f), new Vector2(325.0f, 250.0f)};
   public Vector2[] start_directions =
-      {new Vector2(-1.0f, 0.0f), new Vector2(1.0f, 0.0f),};
+      {new Vector2(-1.0f, 0.0f), new Vector2(1.0f, 0.0f),
+          new Vector2(0.0f, 1.0f), new Vector2(0.0f, -1.0f),};
 
   public String floor_texture = "floor_stone_small.png";
   public int[] board_dimensions = {1000, 1000};
