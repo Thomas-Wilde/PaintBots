53ac72c9bd937f647102f23cde1679dc8c22e4f2
Thomas Wilde <ThomasWilde1984@gmail.com>
2021-11-26 22:16:54 +0100

    CHANGED: set window size on start up

--- desktop/src/com/tw/paintbots/desktop/DesktopLauncher.java
+++ desktop/src/com/tw/paintbots/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.tw.paintbots.PaintBotsGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PaintBotsGame(), config);
-	}
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+    config.title = "Paint Bots";
+    config.height = 1000;
+    config.width = 1500;
+    new LwjglApplication(new PaintBotsGame(), config);
+  }
 }
