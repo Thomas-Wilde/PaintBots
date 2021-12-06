9a59592a357873bc1bc3cd3f9ea474196c30547a
Thomas Wilde <ThomasWilde1984@gmail.com>
2021-12-07 00:37:57 +0100

    MINOR: changed start positions in GameSettings

--- core/src/com/tw/paintbots/GameSettings.java
+++ core/src/com/tw/paintbots/GameSettings.java
@@ -6,8 +6,8 @@ public class GameSettings {
   public PlayerType[] player_types =
       {PlayerType.HUMAN, PlayerType.HUMAN, PlayerType.HUMAN, PlayerType.HUMAN};
   public Vector2[] start_positions =
-      {new Vector2(250.0f, 500.0f), new Vector2(500.0f, 500.0f),
-          new Vector2(325.0f, 750.0f), new Vector2(325.0f, 250.0f)};
+      {new Vector2(250.0f, 500.0f), new Vector2(750.0f, 500.0f),
+          new Vector2(500.0f, 750.0f), new Vector2(500.0f, 250.0f)};
   public Vector2[] start_directions =
       {new Vector2(-1.0f, 0.0f), new Vector2(1.0f, 0.0f),
           new Vector2(0.0f, 1.0f), new Vector2(0.0f, -1.0f),};
@@ -16,5 +16,5 @@ public class GameSettings {
   public String floor_texture = "floor_stone_medium.png";
   public int[] board_dimensions = {1000, 1000};
   public int[] board_border = {100, 100};
-  public int ui_width = 300;
+  public int ui_width = 400;
 }
