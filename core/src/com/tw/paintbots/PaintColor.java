196e2ff75363e2a873f7c2aef2032b4ddb1a3f3f
Thomas Wilde <ThomasWilde1984@gmail.com>
2021-12-10 13:49:05 +0100

    minor: formatting

--- core/src/com/tw/paintbots/PaintColor.java
+++ core/src/com/tw/paintbots/PaintColor.java
@@ -8,40 +8,21 @@ public enum PaintColor {
   private final int color_id;
   private final Color color;
 
+  //@formatter:off
   // --------------------------------------------------------------- //
-  PaintColor(int color_id) {
+  private PaintColor(int color_id) {
     this.color_id = color_id;
     switch (color_id) {
-      // ---
-      case 0: // green
-        color = new Color(0.360f, 0.560f, 0.280f, 0.750f); // [92, 143. 71]
-        break;
-      // ---
-      case 1: // purple
-        color = new Color(0.560f, 0.450f, 0.690f, 0.750f); // [143, 115, 176]
-        break;
-      // ---
-      case 2: // blue
-        color = new Color(0.400f, 0.600f, 0.810f, 0.750f); // [102, 153, 207]
-        break;
-      // ---
-      case 3: // orange
-        color = new Color(0.830f, 0.440f, 0.370f, 0.750f); // [212, 112, 94]
-        break;
-      // ---
-      default: // black
-        color = new Color(0.078f, 0.059f, 0.043f, 0.750f); // [20, 15, 11]
-        break;
+      case 0:  color = new Color(0.360f, 0.560f, 0.280f, 0.750f); break; //green  [ 92, 143,  71]
+      case 1:  color = new Color(0.560f, 0.450f, 0.690f, 0.750f); break; //purple [143, 115, 176]
+      case 2:  color = new Color(0.400f, 0.600f, 0.810f, 0.750f); break; //blue   [102, 153, 207]
+      case 3:  color = new Color(0.830f, 0.440f, 0.370f, 0.750f); break; //orange [212, 112,  94]
+      default: color = new Color(0.078f, 0.059f, 0.043f, 0.750f); break; //black  [ 20,  15,  11]
     }
   }
 
   // --------------------------------------------------------------- //
-  public int getColorID() {
-    return color_id;
-  }
-
-  // --------------------------------------------------------------- //
-  public Color getColor() {
-    return color;
-  }
+  public int   getColorID() { return color_id; }
+  public Color getColor()   { return color; }
+  //@formatter:on
 }
