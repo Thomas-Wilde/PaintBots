602c72daa0864ac4ca292438b5fc7b8f43970199
Thomas Wilde <ThomasWilde1984@gmail.com>
2021-12-16 21:57:11 +0100

    ADD: Array utility class

---
+++ core/src/com/tw/paintbots/Array.java
@@ -0,0 +1,9 @@
+package com.tw.paintbots;
+
+// Utility class
+class Array {
+  // --------------------------------------------------------------- //
+  public static int[] of(int... elements) {
+    return elements;
+  }
+}
