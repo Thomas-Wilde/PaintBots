542a785381d4cf73fc9915cb76c1f131209cf86d
Thomas Wilde <ThomasWilde1984@gmail.com>
2021-12-17 00:16:14 +0100

    more Array utility classes

--- core/src/com/tw/paintbots/Array.java
+++ core/src/com/tw/paintbots/Array.java
@@ -6,4 +6,14 @@ class Array {
   public static int[] of(int... elements) {
     return elements;
   }
+
+  // --------------------------------------------------------------- //
+  public static String[] of(String... elements) {
+    return elements;
+  }
+
+  // --------------------------------------------------------------- //
+  public static float[] of(float... elements) {
+    return elements;
+  }
 }
