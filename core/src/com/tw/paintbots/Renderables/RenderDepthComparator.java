28bbfa6267867ec4ab5e1d3d5f56a3a8269b504c
Thomas Wilde <ThomasWilde1984@gmail.com>
2022-01-24 16:34:03 +0100

    ADD: DepthComparator - used for depth occlusion

---
+++ core/src/com/tw/paintbots/Renderables/RenderDepthComparator.java
@@ -0,0 +1,20 @@
+package com.tw.paintbots.Renderables;
+
+import java.util.Comparator;
+
+public class RenderDepthComparator implements Comparator<Renderable> {
+  @Override
+  public int compare(Renderable r1, Renderable r2) {
+    // ---
+    if (r1 == null && r2 == null)
+      return 0;
+    if (r1 == null)
+      return 1;
+    if (r2 == null)
+      return -1;
+    // ---
+    int depth1 = r1.getRenderDepth() + r1.getRenderPosition()[1];
+    int depth2 = r2.getRenderDepth() + r2.getRenderPosition()[1];
+    return depth2 - depth1;
+  }
+}
