feb34e58a45d92cfbecec48d98b034be31e45845
Thomas Wilde <ThomasWilde1984@gmail.com>
2022-01-25 11:24:19 +0100

    CHANGED: Items now have a texture and an interaction texture, that is loaded in the Item class automatically

--- core/src/com/tw/paintbots/Items/Item.java
+++ core/src/com/tw/paintbots/Items/Item.java
@@ -1,22 +1,36 @@
 package com.tw.paintbots.Items;
 
-import com.tw.paintbots.Renderables.MultiLayerRenderable;
+import java.util.Objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Color;
+
+import com.tw.paintbots.Renderables.SimpleRenderable;
 import com.tw.paintbots.GameManager.SecretKey;
 
-public abstract class Item extends MultiLayerRenderable {
-  private ItemType type = ItemType.NONE;
+import com.tw.paintbots.Array;
+
+public abstract class Item extends SimpleRenderable {
+  String area_file = "";
+  private ItemArea area;
 
   // ======================== Item methods ========================= //
-  protected Item(String name, int[] layers, String[] texture_files,
-      ItemType type) {
-    super(name, layers, texture_files);
-    this.type = type;
-  }
-
-  // --------------------------------------------------------------- //
   //@formatter:off
-  public ItemType getType() { return type; }
-  //@formatter:on
+  protected Item(String name,
+                 String texture_file, String area_file,
+                 float[] scale, int occlusion_depth) { //@formatter:on
+    super(name, 20, texture_file);
+    this.area_file = area_file;
+    setScale(scale);
+    // --- occlusion depth
+    setOcclusionDepth((int) (occlusion_depth * scale[1]));
+    int width = texture.getWidth();
+    int height = texture.getHeight();
+    setRenderOffset(Array.of(-width / 2, -height / 2));
+  }
 
   // --------------------------------------------------------------- //
   public void init() {
@@ -24,8 +38,53 @@ public abstract class Item extends MultiLayerRenderable {
   }
 
   // --------------------------------------------------------------- //
-  //@formatter:off
-  public abstract ItemArea getItemArea(SecretKey key);
-  protected abstract void initItemArea();
-  //@formatter:on
+  protected void initItemArea() {
+    // --- prepare values
+    Vector2 pos = getPosition();
+    float[] scale = getScale();
+    int[] offset = getRenderOffset();
+    int[] origin = new int[] {(int) (pos.x + (offset[0] * scale[0])),
+        (int) (pos.y + (offset[1] * scale[1]))};
+    int width = (int) (texture.getWidth() * scale[0]);
+    int height = (int) (texture.getHeight() * scale[1]);
+    // --- create area
+    area = new ItemArea(width, height);
+    area.setOrigin(origin);
+    Pixmap area_data = loadItemAreaData();
+    transferDataToArea(area_data);
+
+  }
+
+  // --------------------------------------------------------------- //
+  private Pixmap loadItemAreaData() {
+    Texture area_texture = new Texture(Gdx.files.internal(area_file));
+    if (!area_texture.getTextureData().isPrepared())
+      area_texture.getTextureData().prepare();
+    // --- transfer texture to area
+    Pixmap pixmap = area_texture.getTextureData().consumePixmap();
+    area_texture.dispose();
+    return pixmap;
+  }
+
+  // --------------------------------------------------------------- //
+  private void transferDataToArea(Pixmap area_data) {
+    int width = area.getWidth();
+    int height = area.getHeight();
+    float[] scale = getScale();
+    // --- load interaction texture
+    for (int i = 0; i < width; ++i)
+      for (int j = 0; j < height; ++j) {
+        int x = (int) (i / scale[0]);
+        int y = (int) (j / scale[1]);
+        Color col = new Color(area_data.getPixel(x, y));
+        int type_id = (int) (col.r * 255.0f); // red channel encodes item type
+        area.setType(i, height - 1 - j, type_id); // image is upside-down
+      }
+  }
+
+  // --------------------------------------------------------------- //
+  public ItemArea getItemArea(SecretKey key) {
+    Objects.requireNonNull(key);
+    return area;
+  }
 }
--- core/src/com/tw/paintbots/Items/PaintBooth.java
+++ core/src/com/tw/paintbots/Items/PaintBooth.java
@@ -1,57 +1,13 @@
 package com.tw.paintbots.Items;
 
-import java.util.Objects;
-import com.badlogic.gdx.math.Vector2;
 import com.tw.paintbots.Array;
-import com.tw.paintbots.GameManager.SecretKey;
 
 // =============================================================== //
+/** The PaintBooth is an obstacle with a border to refill the paint. */
 public class PaintBooth extends Item {
-  /** Magic values specific for this item. */
-  private int[] area_offset = {40, 60};
-  private int[] dim = {464, 260};
-  private ItemArea area;
-
   // ===================== PaintBooth methods ====================== //
   public PaintBooth() {
-    super("PaintBooth", Array.of(6, 4),
-        Array.of("paint_booth_a.png", "paint_booth_b.png"), ItemType.REFILL);
-    // ---
-    setRenderOffset(Array.of(-dim[0] / 2, -dim[1] / 2));
-    setScale(Array.of(0.57f, 0.57f));
-  }
-
-  // --------------------------------------------------------------- //
-  /** The PaintBooth is an obstacle with a border to refill the paint. */
-  @Override
-  protected void initItemArea() {
-    // ---
-    float[] scale = getScale();
-    int width = (int) (dim[0] * scale[0]) + 2 * area_offset[0];
-    int height = (int) (dim[1] * scale[1]) + 2 * area_offset[1];
-    area = new ItemArea(width, height);
-    // ---
-    Vector2 origin = getPosition();
-    origin.x -= width / 2.0f;
-    origin.y -= height / 2.0f;
-    area.setOrigin(Array.of((int) origin.x, (int) origin.y));
-    // ---
-    for (int x = 0; x < area.getWidth(); ++x)
-      for (int y = 0; y < area.getHeight(); ++y) {
-        area.setType(x, y, ItemType.REFILL);
-      }
-    int off_x = area_offset[0];
-    int off_y = area_offset[1];
-    for (int x = off_x / 2; x < area.getWidth() - off_x / 2; ++x)
-      for (int y = off_y / 2; y < area.getHeight() - off_y; ++y) {
-        area.setType(x, y, ItemType.OBSTACLE);
-      }
-  }
-
-  // ========================= Item methods ======================== //
-  @Override
-  public ItemArea getItemArea(SecretKey key) {
-    Objects.requireNonNull(key);
-    return area;
+    super("PaintBooth", "paint_booth.png", "paint_booth_area.png",
+        Array.of(0.65f, 0.65f), 120);
   }
 }
