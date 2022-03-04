bef93f24515de6ac20c1c202e8eb0e6c7b86da9b
Thomas Wilde <thomas@isg.cs.ovgu.de>
2022-03-04 22:26:05 +0100

    CHANGED: switch to lwjgl3

--- desktop/src/com/tw/paintbots/desktop/DesktopLauncher.java
+++ desktop/src/com/tw/paintbots/desktop/DesktopLauncher.java
@@ -1,17 +1,18 @@
 package com.tw.paintbots.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.tw.paintbots.PaintBotsGame;
 
 public class DesktopLauncher {
   public static void main(String[] arg) {
     // ---
-    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-    config.title = "Paint Bots";
-    config.height = 1000;
-    config.width = 1500;
-    config.foregroundFPS = 60;
-    new LwjglApplication(new PaintBotsGame(), config);
+    Lwjgl3ApplicationConfiguration config =
+        new Lwjgl3ApplicationConfiguration();
+    config.setWindowedMode(1500, 1000);
+    config.setIdleFPS(60);
+    config.setForegroundFPS(60);
+    // config.setInitialVisible(false);
+    new Lwjgl3Application(new PaintBotsGame(), config);
   }
 }
