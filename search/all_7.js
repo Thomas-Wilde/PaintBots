9f859bf8799307db9af7f339f0a898d361503af6
Thomas-Wilde <Thomas-Wilde@users.noreply.github.com>
2022-03-25 23:52:02 +0000

    deploy: ee6e67e1070a6a17730a007e75a5d478820f1ba2

--- search/all_7.js
+++ search/all_7.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['ispaintable_0',['isPaintable',['../enumcom_1_1tw_1_1paintbots_1_1_items_1_1_item_type.html#abf71d597bd80f7bab33e7448115a97b6',1,'com::tw::paintbots::Items::ItemType']]],
   ['ispassable_1',['isPassable',['../enumcom_1_1tw_1_1paintbots_1_1_items_1_1_item_type.html#a51cb17d2d771a595028eace2678f867c',1,'com::tw::paintbots::Items::ItemType']]],
-  ['item_2',['Item',['../classcom_1_1tw_1_1paintbots_1_1_items_1_1_item.html',1,'com::tw::paintbots::Items']]],
-  ['itemarea_3',['ItemArea',['../classcom_1_1tw_1_1paintbots_1_1_items_1_1_item_area.html',1,'com::tw::paintbots::Items']]],
-  ['itemproperties_4',['ItemProperties',['../classcom_1_1tw_1_1paintbots_1_1_items_1_1_item_properties.html',1,'com::tw::paintbots::Items']]],
-  ['itemtype_5',['ItemType',['../enumcom_1_1tw_1_1paintbots_1_1_items_1_1_item_type.html',1,'com::tw::paintbots::Items']]]
+  ['isvisible_2',['isVisible',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_renderable.html#a844d1cbf876c2359cf454c7f8af2531d',1,'com::tw::paintbots::Renderables::Renderable']]],
+  ['item_3',['Item',['../classcom_1_1tw_1_1paintbots_1_1_items_1_1_item.html',1,'com::tw::paintbots::Items']]],
+  ['itemarea_4',['ItemArea',['../classcom_1_1tw_1_1paintbots_1_1_items_1_1_item_area.html',1,'com::tw::paintbots::Items']]],
+  ['itemproperties_5',['ItemProperties',['../classcom_1_1tw_1_1paintbots_1_1_items_1_1_item_properties.html',1,'com::tw::paintbots::Items']]],
+  ['itemtype_6',['ItemType',['../enumcom_1_1tw_1_1paintbots_1_1_items_1_1_item_type.html',1,'com::tw::paintbots::Items']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['ispaintable_0',['isPaintable',['../enumcom_1_1tw_1_1paintbots_1_1_items_1_1_item_type.html#abf71d597bd80f7bab33e7448115a97b6',1,'com::tw::paintbots::Items::ItemType']]],
-  ['ispassable_1',['isPassable',['../enumcom_1_1tw_1_1paintbots_1_1_items_1_1_item_type.html#a51cb17d2d771a595028eace2678f867c',1,'com::tw::paintbots::Items::ItemType']]]
+  ['ispassable_1',['isPassable',['../enumcom_1_1tw_1_1paintbots_1_1_items_1_1_item_type.html#a51cb17d2d771a595028eace2678f867c',1,'com::tw::paintbots::Items::ItemType']]],
+  ['isvisible_2',['isVisible',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_renderable.html#a844d1cbf876c2359cf454c7f8af2531d',1,'com::tw::paintbots::Renderables::Renderable']]]
 ];
