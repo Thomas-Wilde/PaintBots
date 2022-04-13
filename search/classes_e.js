b9e952092fcb838a80aead54af027a653fe6a2fe
Thomas-Wilde <Thomas-Wilde@users.noreply.github.com>
2022-04-13 20:51:35 +0000

    deploy: a4efeb088130c458ae053f64323299cb2afe526c

--- search/classes_e.js
+++ search/classes_e.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['uicolorbar_0',['UIColorBar',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_color_bar.html',1,'com::tw::paintbots::Renderables']]],
-  ['uidigit_1',['UIDigit',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_digit.html',1,'com::tw::paintbots::Renderables']]],
-  ['uiplayerboard_2',['UIPlayerBoard',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_player_board.html',1,'com::tw::paintbots::Renderables']]],
-  ['uitimer_3',['UITimer',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_timer.html',1,'com::tw::paintbots::Renderables']]]
+  ['uibotselect_0',['UIBotSelect',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_bot_select.html',1,'com::tw::paintbots::Renderables']]],
+  ['uicolorbar_1',['UIColorBar',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_color_bar.html',1,'com::tw::paintbots::Renderables']]],
+  ['uidigit_2',['UIDigit',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_digit.html',1,'com::tw::paintbots::Renderables']]],
+  ['uiplayerboard_3',['UIPlayerBoard',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_player_board.html',1,'com::tw::paintbots::Renderables']]],
+  ['uitimer_4',['UITimer',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_timer.html',1,'com::tw::paintbots::Renderables']]]
 ];
