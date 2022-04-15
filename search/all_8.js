7eb4e55e8bb2b6aa8ed2b2fd2b62b14eca2deec3
Thomas-Wilde <Thomas-Wilde@users.noreply.github.com>
2022-04-15 22:42:28 +0000

    deploy: f80dd70c632d034cc737513370832f23e435c723

--- search/all_8.js
+++ search/all_8.js
@@ -3,5 +3,6 @@ var searchData=
   ['level_0',['Level',['../classcom_1_1tw_1_1paintbots_1_1_level.html',1,'com::tw::paintbots']]],
   ['levelloader_1',['LevelLoader',['../classcom_1_1tw_1_1paintbots_1_1_level_loader.html',1,'com::tw::paintbots']]],
   ['loadbots_2',['loadBots',['../classcom_1_1tw_1_1paintbots_1_1_bot_loader.html#a473ad6a1ad940513a08a89759ba22de9',1,'com::tw::paintbots::BotLoader']]],
-  ['loadlevelfiles_3',['loadLevelFiles',['../classcom_1_1tw_1_1paintbots_1_1_level_loader.html#a2ea80a10db81fee3ad027822b1a8447d',1,'com::tw::paintbots::LevelLoader']]]
+  ['loadlevelfiles_3',['loadLevelFiles',['../classcom_1_1tw_1_1paintbots_1_1_level_loader.html#a2ea80a10db81fee3ad027822b1a8447d',1,'com::tw::paintbots::LevelLoader']]],
+  ['loadmenu_4',['loadMenu',['../classcom_1_1tw_1_1paintbots_1_1_game_manager.html#a671dad82d71b5e7371e0072a89be2631',1,'com::tw::paintbots::GameManager']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['loadbots_0',['loadBots',['../classcom_1_1tw_1_1paintbots_1_1_bot_loader.html#a473ad6a1ad940513a08a89759ba22de9',1,'com::tw::paintbots::BotLoader']]],
-  ['loadlevelfiles_1',['loadLevelFiles',['../classcom_1_1tw_1_1paintbots_1_1_level_loader.html#a2ea80a10db81fee3ad027822b1a8447d',1,'com::tw::paintbots::LevelLoader']]]
+  ['loadlevelfiles_1',['loadLevelFiles',['../classcom_1_1tw_1_1paintbots_1_1_level_loader.html#a2ea80a10db81fee3ad027822b1a8447d',1,'com::tw::paintbots::LevelLoader']]],
+  ['loadmenu_2',['loadMenu',['../classcom_1_1tw_1_1paintbots_1_1_game_manager.html#a671dad82d71b5e7371e0072a89be2631',1,'com::tw::paintbots::GameManager']]]
 ];
