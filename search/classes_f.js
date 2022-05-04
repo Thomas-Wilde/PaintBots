7bc3843371a625d7d46c9af5ceb4aa4a3575706f
Thomas-Wilde <Thomas-Wilde@users.noreply.github.com>
2022-05-04 14:53:28 +0000

    deploy: 629e756e3dc9d5512a0afc9ded02431bbf9646f0

--- search/classes_f.js
+++ search/classes_f.js
@@ -4,5 +4,6 @@ var searchData=
   ['uidigit_1',['UIDigit',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_digit.xhtml',1,'com::tw::paintbots::Renderables']]],
   ['uimenuitem_2',['UIMenuItem',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_menu_item.xhtml',1,'com::tw::paintbots::Renderables']]],
   ['uiplayerboard_3',['UIPlayerBoard',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_player_board.xhtml',1,'com::tw::paintbots::Renderables']]],
-  ['uitimer_4',['UITimer',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_timer.xhtml',1,'com::tw::paintbots::Renderables']]]
+  ['uiresultboard_4',['UIResultBoard',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_result_board.xhtml',1,'com::tw::paintbots::Renderables']]],
+  ['uitimer_5',['UITimer',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_u_i_timer.xhtml',1,'com::tw::paintbots::Renderables']]]
 ];
