26b89b2601556c19e87bb9a201653dc82a564808
Thomas-Wilde <Thomas-Wilde@users.noreply.github.com>
2022-05-25 10:53:36 +0000

    deploy: 63915aed01266fa7fb7321681d1817718d8326da

--- search/pages_0.js
+++ search/pages_0.js
@@ -3,5 +3,8 @@ var searchData=
   ['paint_20bots_20_2d_20das_20spielfeld_20_26_20eigene_20levels_0',['Paint Bots - Das Spielfeld &amp; Eigene Levels',['../md_docs_levels.xhtml',1,'']]],
   ['paint_20bots_20_2d_20ein_20eigener_20bot_1',['Paint Bots - Ein eigener Bot',['../md_docs_bot.xhtml',1,'']]],
   ['paint_20bots_20_2d_20es_20geht_20los_21_2',['Paint Bots - Es geht los!',['../md_docs_intro.xhtml',1,'']]],
-  ['paintbots_3',['PaintBots',['../index.xhtml',1,'']]]
+  ['paintbots_3',['PaintBots',['../index.xhtml',1,'']]],
+  ['paintbots_20_2d_20gegenstände_20auf_20dem_20feld_4',['PaintBots - Gegenstände auf dem Feld',['../md_docs_items.xhtml',1,'']]],
+  ['paintbots_20_2d_20klausurzulassung_2c_20regeln_20_26_20wettbewerb_5',['PaintBots - Klausurzulassung, Regeln &amp; Wettbewerb',['../md_docs_task.xhtml',1,'']]],
+  ['paintbots_20_2d_20powerups_6',['PaintBots - PowerUps',['../md_docs_powerups.xhtml',1,'']]]
 ];
