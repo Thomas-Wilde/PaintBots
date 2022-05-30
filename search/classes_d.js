458fa864978d328cd1150d2c385f21b1ea3ada34
Thomas-Wilde <Thomas-Wilde@users.noreply.github.com>
2022-05-30 16:56:38 +0000

    deploy: 142205f367e5fd91a242f50fc01a81e01460189e

--- search/classes_d.js
+++ search/classes_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['simplerenderable_0',['SimpleRenderable',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_simple_renderable.xhtml',1,'com::tw::paintbots::Renderables']]],
-  ['starttimer_1',['StartTimer',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_start_timer.xhtml',1,'com::tw::paintbots::Renderables']]]
+  ['settingsloader_0',['SettingsLoader',['../classcom_1_1tw_1_1paintbots_1_1_settings_loader.xhtml',1,'com::tw::paintbots']]],
+  ['simplerenderable_1',['SimpleRenderable',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_simple_renderable.xhtml',1,'com::tw::paintbots::Renderables']]],
+  ['starttimer_2',['StartTimer',['../classcom_1_1tw_1_1paintbots_1_1_renderables_1_1_start_timer.xhtml',1,'com::tw::paintbots::Renderables']]]
 ];
