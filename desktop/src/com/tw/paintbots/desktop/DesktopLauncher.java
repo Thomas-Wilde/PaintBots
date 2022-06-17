621ec382f94357007ecfa1eaf959c7a453a8fe8d
Thomas Wilde <ThomasWilde1984@gmail.com>
2022-06-17 16:29:52 +0200

    version

--- desktop/src/com/tw/paintbots/desktop/DesktopLauncher.java
+++ desktop/src/com/tw/paintbots/desktop/DesktopLauncher.java
@@ -93,7 +93,7 @@ public class DesktopLauncher {
   // --------------------------------------------------------------- //
   public static void main(String[] arg) {
     // ===================== //
-    String version = "0.06.15";
+    String version = "0.06.17";
     // ===================== //
     // ---
     if (argContains(arg, "-version")) {
